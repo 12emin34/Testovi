955b2a08fbf0926fe3b33d04ed4a6bf270ba3d49
Jupol250ml <dino.saric.skola@gmail.com>
2023-11-03 12:32:12 +0100

    igranje(nije bitno)

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ let pitanje1 = new Pitanje(new Map([
     ["odg1", true],
     ["odg2", false],
     ["odg3", true],
+    ["odg4", false],
 ]))
 
-if (pitanje1.odgovori.get("odg1")) {
+if (pitanje1.odgovori.get("odg2")) {
     console.log("dzaja")
+} else {
+    console.log("ilhan je pametan")
 }
