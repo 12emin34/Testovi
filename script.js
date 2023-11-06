79d71a1bc64402b5399a741cfdb127e2b3d109a8
Jupol250ml <dino.saric.skola@gmail.com>
2023-11-06 08:07:24 +0100

    Malo proširivanje

--- script.js
+++ script.js
@@ -5,14 +5,30 @@ class Pitanje {
 }
 
 let pitanje1 = new Pitanje(new Map([
-    ["odg1", true],
+    ["odg1", false],
     ["odg2", false],
     ["odg3", true],
     ["odg4", false],
 ]))
 
-if (pitanje1.odgovori.get("odg2")) {
-    console.log("dzaja")
-} else {
-    console.log("ilhan je pametan")
+if(pitanje1.odgovori.get("odg3")){
+    console.log("Tačno!");
+}
+
+let pitanje2 = new Pitanje(new Map([
+    ["odg1", true],
+    ["odg2", false],
+    ["odg3", false],
+    ["odg4", false],
+    ["odg5", false],
+    ["odg6", false],
+    ["odg7", false],
+    ["odg8", false],
+    ["odg9", false],
+    ["odg10", false],
+    ["odg11", false],
+]))
+
+if(pitanje2.odgovori.get("odg1")){
+    console.log("Tačno!");
 }
