14194977f8615b4ee2cf6baa8a242fdc9abf3ba6
12emin34 <macanovic.emin@gmail.com>
2023-11-07 10:48:57 +0100

    napredak na JS frontu, dodane metode za pitanja

--- script.js
+++ script.js
@@ -1,34 +1,24 @@
 class Pitanje {
-    constructor(odgovori) {
-        this.odgovori = odgovori;
+    #odgovori = new Map();
+
+    constructor(nizOdgovora) {
+        this.#odgovori = new Map(nizOdgovora)
+    }
+
+    get odgovori() {
+        return this.#odgovori
+    }
+
+    set odgovori(nizOdgovora) {
+        this.#odgovori = new Map(nizOdgovora)
     }
 }
 
-let pitanje1 = new Pitanje(new Map([
-    ["odg1", false],
-    ["odg2", false],
-    ["odg3", true],
-    ["odg4", false],
-]))
+let pitanje1 = new Pitanje([
+        ["odg1", true]
+    ]
+)
 
-if(pitanje1.odgovori.get("odg3")){
-    console.log("Tačno!");
-}
+pitanje1.odgovori = [["odg1", true], ["odg2", false]]
 
-let pitanje2 = new Pitanje(new Map([
-    ["odg1", true],
-    ["odg2", false],
-    ["odg3", false],
-    ["odg4", false],
-    ["odg5", false],
-    ["odg6", false],
-    ["odg7", false],
-    ["odg8", false],
-    ["odg9", false],
-    ["odg10", false],
-    ["odg11", false],
-]))
-
-if(pitanje2.odgovori.get("odg1")){
-    console.log("Tačno!");
-}
+console.log(pitanje1.odgovori)
