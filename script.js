574ef381c5191bb8523fcb1b527b18b4816fbf88
12emin34 <macanovic.emin@gmail.com>
2023-11-14 10:50:30 +0100

    stub za pitanja i odgovore na stranici

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
+/**
+ * Predstavlja pitanje zajedno sa njegovim odgovorima
+ * @constructor
+ * @param {array} nizOdgovora
+ */
 class Pitanje {
+    /**
+     * Predstavlja kolekciju parova odgovora i njihove tačnosti u obliku Map objekta
+     * @type {Map<any, any>}
+     */
     #odgovori = new Map();
 
     constructor(nizOdgovora) {
         this.#odgovori = new Map(nizOdgovora)
     }
 
+    /**
+     * Vraća sve odgovore od pitanja u obliku Map objekta
+     * @returns {Map<any, any>}
+     */
     get odgovori() {
         return this.#odgovori
     }
 
+    /**
+     * Postavlja odgovore za pitanje
+     * @param nizOdgovora
+     */
     set odgovori(nizOdgovora) {
         this.#odgovori = new Map(nizOdgovora)
     }
 }
 
-let pitanje1 = new Pitanje([
-        ["odg1", true]
-    ]
-)
-
-pitanje1.odgovori = [["odg1", true], ["odg2", false]]
-
-console.log(pitanje1.odgovori)
+let pitanje1 = new Pitanje()
