855a93e3dff8c5ad7b13ed926fe4b7c1a20b3202
Jupol250ml <dino.saric.skola@gmail.com>
2023-11-17 13:11:49 +0100

    Funkcija za dodavanje pitanja(WIP)

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
+var brojPitanja = 2;
+
 /**
  * Predstavlja pitanje zajedno sa njegovim odgovorima
  * @constructor
  * @param {array} nizOdgovora
  */
 class Pitanje {
+    #pitanje = "";
+
     /**
      * Predstavlja kolekciju parova odgovora i njihove tačnosti u obliku Map objekta
      * @type {Map<string, boolean>}
      */
     #odgovori = new Map();
 
-    constructor(nizOdgovora) {
+    constructor(pitanje, nizOdgovora) {
+        this.#pitanje = pitanje 
         this.#odgovori = new Map(nizOdgovora)
     }
 
@@ -22,6 +27,10 @@ class Pitanje {
         return this.#odgovori
     }
 
+    get pitanje() {
+        return this.#pitanje
+    }
+
     /**
      * Postavlja odgovore za pitanje
      * @param nizOdgovora
@@ -29,33 +38,73 @@ class Pitanje {
     set odgovori(nizOdgovora) {
         this.#odgovori = new Map(nizOdgovora)
     }
+
+    set pitanje(pitanje){
+        this.#pitanje = pitanje
+    }
+
+    brojOdgovora(){
+        return this.#odgovori.size;
+    }
 }
 
-
-/*
-let pitanje1 = new Pitanje([["odgovor 1", true], ["odgovor 2", false]])
-console.log(pitanje1.odgovori)
-
-let noviElement = document.createElement("h1")
-noviElement.innerText = "dzaja"
-document.body.appendChild(noviElement)
-*/
 function novoPitanje(pitanje) {
     /** 
-     *varijabla koja predstavlja div u kojem se nalaze pitanje i odgovori 
+     *varijabla koja predstavlja glavni div
     */
     let glavniBox = document.getElementById("box")
 
-    let divPitanje = document.createElement("div")
-    divPitanje.className = "box-pitanje"
+    /**
+     * varijabla koja predstavlja div u kojem se nalaze pitanje i odgovori
+     */
+    let divPitanjeIOdg = document.createElement("div")
+    divPitanjeIOdg.className = "box-pitanje"
+    glavniBox.appendChild(divPitanjeIOdg)
 
-    glavniBox.appendChild(divPitanje)
+    let divPitanje = document.createElement("div")
+    divPitanje.className = "pitanje"
+    divPitanjeIOdg.appendChild(divPitanje);
+
+    let divRedniBr = document.createElement("div")
+    divRedniBr.className = "rednibroj"
+    divPitanje.appendChild(divRedniBr);
+
+    let hBrojPitanja = document.createElement("h4")
+    divRedniBr.appendChild(hBrojPitanja)
+    hBrojPitanja.innerText = brojPitanja;
+    brojPitanja++;
+
+    let pPitanje = document.createElement("h4")
+    divPitanje.appendChild(pPitanje)
+    pPitanje.innerText = pitanje.pitanje
+
+    let divOdgovori = document.createElement("div")
+    divOdgovori.className = "odgovori"
+    divPitanjeIOdg.appendChild(divOdgovori)
+
+    for (const [key, value] of pitanje.odgovori) {
+        let odabraniOdgovor = document.createElement("input")
+        odabraniOdgovor.type = "checkbox"
+
+        let odgovor = document.createElement("label")
+        odgovor.innerText = key
+
+        let brOdgovori = document.createElement("br")
+
+        divOdgovori.appendChild(odabraniOdgovor)
+        divOdgovori.appendChild(odgovor)
+        divOdgovori.appendChild(brOdgovori)
+    }
+
+
 
 }
 
-let pitanje1 = new Pitanje([["odgovor 1", true], ["odgovor 2", false]]);
-let pitanje2 = new Pitanje([["odgovor 1", true], ["odgovor 2", false]]);
-let pitanje3 = new Pitanje([["odgovor 1", true], ["odgovor 2", false]]);
+let pitanje1 = new Pitanje("Kako se naziva uzdužni dio kolovoza namijenjen za saobraćaj vozila u jednom smijeru sa jednom ili više saobraćajnih traka? ", [["odgovor 1", true], ["odgovor 2", false]]);
+let pitanje2 = new Pitanje("Mami", [["odgovor 1", true], ["odgovor 2", false]]);
+let pitanje3 = new Pitanje("Da napravi jesti", [["odgovor 1", true], ["odgovor 2", false], ["odgovor3", false] , ["odgovor4", false]]);
+
+
 novoPitanje(pitanje1);
 novoPitanje(pitanje2);
 novoPitanje(pitanje3)
